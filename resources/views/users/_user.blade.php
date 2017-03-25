065abd8f272893843515e4323afcad857a2f1447
shen yi fei <m809745357@gmail.com>
2017-03-25 09:14:52 +0000

    bug-1

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -4,9 +4,9 @@
 
 
   @can('destroy', $user)
-    <form action="{{ route('users.destroy', $this->id) }}" method="post">
+    <form action="{{ route('users.destroy', $user->id) }}" method="post">
       {{ csrf_field() }}
-      {{ method_field }}
+      {{ method_field('DELETE') }}
       <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
     </form>
   @endcan
